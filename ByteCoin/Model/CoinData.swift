d17c6c0fd0206dfb0775f00b6f4ddd673d40cf4a
Matthew Owen <MattOwen333@gmail.com>
2024-06-01 05:25:58 -0400

    delegate design pattern

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Carrie Curtis on 5/13/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
+
